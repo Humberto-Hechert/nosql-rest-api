c9efc878170340e308352804bf4776f5a0767957
Humberto Hechert <humberto.luis@pontaltech.com.br>
2023-06-20 23:14:17 -0300

    fix: postPlayer controller validation name and country

--- src/controllers/playersController.js
+++ src/controllers/playersController.js
@@ -8,7 +8,7 @@ class PlayerController {
             res.status(200).json(foundPlayers)
         } catch (err) {
             console.error(err)
-            res.status(500).send({message: `${err.message} - Erro ao buscar jogadores`})
+            res.status(500).send({ message: `${err.message} - Erro ao buscar jogadores` })
         }
     }
 
@@ -18,31 +18,31 @@ class PlayerController {
         players.findById(id)
             .then(foundPlayer => {
                 if (!foundPlayer) {
-                    res.status(404).send({message: "Jogador não encontrado em nossa base"})
+                    res.status(404).send({ message: "Jogador não encontrado em nossa base" })
                 } else {
                     res.status(200).json(foundPlayer)
                 }
             })
-            .catch (err => {
+            .catch(err => {
                 console.error(err)
-                res.status(500).send({message: `${err.message} - Falha ao encontrar jogador`})
+                res.status(500).send({ message: `${err.message} - Falha ao encontrar jogador` })
             })
     }
 
     static updatePlayer = async (req, res) => {
         const { id } = req.params
 
-        players.findByIdAndUpdate(id, {$set: req.body})
+        players.findByIdAndUpdate(id, { $set: req.body })
             .then(updatedPlayer => {
                 if (!updatedPlayer) {
-                    res.status(404).send({message: "Jogador não encontrado em nossa base"})
+                    res.status(404).send({ message: "Jogador não encontrado em nossa base" })
                 } else {
-                    res.status(200).send({message: "Jogador atualizado com sucesso"})
+                    res.status(200).send({ message: "Jogador atualizado com sucesso" })
                 }
             })
-            .catch (err => {
+            .catch(err => {
                 console.error(err)
-                res.status(500).send({message: `${err.message} - Falha ao atualizar jogador`})
+                res.status(500).send({ message: `${err.message} - Falha ao atualizar jogador` })
             })
     }
 
@@ -52,14 +52,14 @@ class PlayerController {
         players.findByIdAndDelete(id)
             .then(deletedPlayer => {
                 if (!deletedPlayer) {
-                    res.status(404).send({message: "Jogador não encontrado em nossa base"})
+                    res.status(404).send({ message: "Jogador não encontrado em nossa base" })
                 } else {
-                    res.status(200).send({message: "Jogador removido"})
+                    res.status(200).send({ message: "Jogador removido" })
                 }
             })
-            .catch (err => {
+            .catch(err => {
                 console.error(err)
-                res.status(500).send({message: `${err.message} - Erro ao remover jogador`})
+                res.status(500).send({ message: `${err.message} - Erro ao remover jogador` })
             })
     }
 
@@ -67,13 +67,18 @@ class PlayerController {
         const player = new players(req.body)
 
         try {
-            player.save()
-                .then(() => {
-                    res.status(200).send(player.toJSON())
-                })
+            if (!player.name || !player.country) {
+                res.status(400).send("Name e country devem ser preenchidos")
+            } else {
+                player.save()
+                    .then(() => {
+                        res.status(200).send(player.toJSON())
+                    })
+            }
+
         } catch (err) {
             console.error(err)
-            res.status(500).send({message : `${err.message} Falha ao cadastrar jogador`})
+            res.status(500).send({ message: `${err.message} Falha ao cadastrar jogador` })
         }
     }
 
